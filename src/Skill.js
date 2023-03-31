e7b8f4756d71305261e6c3b171337493916336f8
Jeremy new computer <jeremy8@vt.edu>
2023-03-31 14:33:02 -0400

    ok

--- src/Skill.js
+++ src/Skill.js
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 function Skill({ directionLeft, src, name }) {
   return (
-    <div className="group p-2 md:p-1 items-center align-middle justify-center text-center h-full cursor-pointer">
+    <div className="group p-2 md:p-1 items-center align-middle justify-center text-center h-full">
       <p className="text-sm text-gray-500 mt-2">{name}</p>
       <motion.img
         initial={{
@@ -13,7 +13,7 @@ function Skill({ directionLeft, src, name }) {
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
         src={src}
-        className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md-h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transtition duration-300 ease-in-out "
+        className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md-h-28 xl:w-32 xl:h-32 filter "
       />
     </div>
   );
