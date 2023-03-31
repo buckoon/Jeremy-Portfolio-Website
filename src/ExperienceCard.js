8d50e6f2cfc6180058537ad0d466ebc63dfbbd28
Jeremy new computer <jeremy8@vt.edu>
2023-03-31 16:28:02 -0400

    updated Workexperience to use props

--- src/ExperienceCard.js
+++ src/ExperienceCard.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { motion } from "framer-motion";
-import Vt from "./images/VT.png";
+/*import Km from "./images/Km.jpg";*/
 
-function ExperienceCard() {
+function ExperienceCard({
+  image,
+  title,
+  company,
+  date,
+  responsibility1,
+  responsibility2,
+  responsibility3,
+  responsibility4,
+  responsibility5,
+}) {
   return (
     <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
       <motion.img
@@ -11,28 +21,23 @@ function ExperienceCard() {
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
         className="h-32 w-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
-        src={Vt}
+        src={image}
       />
       <div className="px-0 md:px-10">
-        <h4 className="text-4xl font-light">Education</h4>
-        <p className="font-bold text-2xl mt-1">Virginia Tech</p>
+        <h4 className="text-4xl font-light">{title}</h4>
+        <p className="font-bold text-2xl mt-1">{company}</p>
         <div className="flex space-x-2 my-2 rounded-full">
           {/* JavaScript Logo <img className="h-10 w-10 rounded-full" src={?.src} /> */}
           {/* <img className="h-10 w-10 rounded-full" src={?.src} /> */}
           {/* <img className="h-10 w-10 rounded-full" src={?.src} /> */}
         </div>
-        <p className="uppercase py-5 text-gray-300">August 2012- June 2015</p>
+        <p className="uppercase py-5 text-gray-300">{date}</p>
         <ul className="list-disc space-y-4 ml-5 text-lg">
-          <li>Bachelors of Science in Mechanical Engineering from Virginia Tech
-          </li>
-          <li>
-                GPA:2.94
-          </li>
-          <li>
-                 President of the Virginia Tech ME Honor Society 2014-2015
-         </li>
-          
-         
+          {responsibility1 && <li>{responsibility1}</li>}
+          {responsibility2 && <li>{responsibility2}</li>}
+          {responsibility3 && <li>{responsibility3}</li>}
+          {responsibility4 && <li>{responsibility4}</li>}
+          {responsibility5 && <li>{responsibility5}</li>}
         </ul>
       </div>
     </article>
--- src/WorkExperience.js
+++ src/WorkExperience.js
@@ -1,10 +1,8 @@
 import React from "react";
-
+import Vt from "./images/VT.png";
+import Km from "./images/Km.jpg";
 import { motion } from "framer-motion";
-import ExperienceCard1 from "./ExperienceCard1";
-import ExperienceCard2 from "./ExperienceCard2";
-import ExperienceCard3 from "./ExperienceCard3";
-import ExperienceCard4 from "./ExperienceCard4";
+import ExperienceCard from "./ExperienceCard";
 import Footer from "./Footer";
 
 
@@ -22,10 +20,55 @@ function WorkExperience() {
 
 
       <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#0A81F7]/80">
-        <ExperienceCard1 />
-        <ExperienceCard2 />
-        <ExperienceCard3 />
-        <ExperienceCard4 />
+        <ExperienceCard
+        image = {Km}
+        title="Oracle Configurator Developer"
+        company="Kollmorgen (a subsidiary of Altra Industrial Motion Corp)"
+        date="June 2022-Current"
+        responsibility1="Design and represent the model Configurator UI(User Interface), configuration logic and BOM (Bill of Material)"
+        responsibilty2="Configure, develop, and maintain database models using Oracle Applications and Configurator
+        Developer with Oracle 11i/R12"
+        responsibility3="Analyzing, generating and maintaining massive amounts of data using 
+        SQL, Excel, Oracle configurator and Data Loader."
+        responsibility4="Implemented automation to reduce Kollmorgen's order backlog by 200% through
+        configuration, development, and maintenance of models using Oracle Applications."
+        responsibility5="Provide technical support and assistance to different departments concerning Oracle and Istore."
+        />
+        <ExperienceCard 
+        image = {Km}
+        title="A&D Sales Project Engineer"
+        company="Kollmorgen (a subsidiary of Altra Industrial Motion Corp)"
+        date="May 2021-June 2022"
+        responsibility1="Directly supported large OEMs in Aerospace & Defense, Medical, and High Tech/Semi-
+        Conductor markets which led to achieving the growth of Kollmorgen direct A&D
+           accounts 10% more than the yearly projection of $xxx million."
+        responsibility2="Accomplished the annual objective of generating a significant number of new prototypes for the US A&D region by exceeding the target by 30%."
+        responsibility3=" Independent project for a solution how to communicate with KAS(Kollmorgen Automation Suite software) using Python through Ethernet IP Via HTTP."
+        responsibility4="Directly overseeing and managing new designs that are based off the customer spec from receiving the initial spec until the product ships."
+        responsibility5=""/>
+
+        <ExperienceCard 
+        image = {Km}
+        title="Application Engineer"
+        company="Kollmorgen (a subsidiary of Altra Industrial Motion Corp)"
+        date="June 2015-April 2021"
+        responsibility1="Contributed to the achievement $16.43 mil in new business for the year 2020"
+        responsibilty2="Worked with customers performing engineering calculations daily in order to come up with a design solution based off customer requirements"
+        responsibility3=" Improved our customer on time responses (less than 2 days) from 34% to 90%"
+        responsibility4="Lead multiple Kaizen events where one resulted in the achievement of the KPI on time delivery improving from 54% to 80% "
+        responsibility5=""/> 
+
+        <ExperienceCard 
+        image = {Vt}
+        title="Education"
+        company="Virginia Tech"
+        date="August 2012- June 2015"
+        responsibility1="Bachelors of Science in Mechanical Engineering from Virginia Tech"
+        responsibilty2="President of the Virginia Tech ME Honor Society 2014-2015"
+        responsibility3=""
+        responsibility4=""
+        responsibility5=""/>    
+       
         
       </div>
       <Footer />
