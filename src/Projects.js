51c7c302197ceeb159db41f6c2e5f356ff40594e
Jeremy new computer <jeremy8@vt.edu>
2023-06-12 18:14:55 -0400

    Updated the description of the new project

--- src/Projects.js
+++ src/Projects.js
@@ -16,9 +16,8 @@ function Projects() {
       imageUrl: Kn,
       title: "MERN Stack Hobby Hunter",
       link: "https://hobby-hunter.onrender.com",
-
       description:
-        "This project is currently under development and represents my second version of Hobby Hunter. I chose to utilize the MERN Stack because it allows me to have greater control over the project's functionality by creating my own backend. Furthermore, I have also redesigned a significant portion of the user interface to enhance its professional appearance." 
+        "This project is currently being developed as the second version of Hobby Hunter. To have more control over the project's functionality, I decided to utilize the MERN Stack, which allows me to create a custom backend. Alongside this, I also redesigned the React portion of the project.\n\nThe concept for this website originated from my aspiration to build something original, rather than replicating existing websites. When my father retired in 2022, he encountered difficulties in finding new hobbies or activities to occupy his time. This served as the inspiration behind Hobby Hunter. It is a web application that empowers users to create personal profiles and share their hobbies, including detailed instructions on how to get started with each one. Built using the MERN Stack (MongoDB, Express, React, Node.js) and incorporating Redux for state management, the application encompasses various features. These include user authentication, the ability to create posts, as well as like and comment on them. Moreover, it includes a weather forecaster and a random hobby generator. Currently, my focus is on implementing the functionality to add friends and the user profile component for each user, which I aim to complete this weekend. Following that, I plan to incorporate a search bar to enable users to query different hobbies.",
     },
     {
       number: 2,
@@ -26,33 +25,8 @@ function Projects() {
       title: "Hobby Hunter",
       link: "https://hobby-hunter-pro.vercel.app",
       description:
-        "After building several clones of websites that already exist I wanted to build something that was my own original concept. When my dad retired in 2022 he was having trouble looking for new things or hobbies to occupy his time and that is when I thought of creating Hobby Hunter. Hobby Hunter is a web application that enables users to create a personal profile and share their hobbies along with instructions on how to get started with a hobby. The app is built using REACT Redux and features Google authentication for secure login and logout. Additionally, the app utilizes firebase storage to store the hobbies that users may post. Even though the App has been deployed to production, it is still a work in progress as I am currently expanding the backend using Node.js, Express, and MongoDB. Building out the backend will provide additional opportunities, such as the ability to modify a user's profile after creation and the ability to engage in conversation with other users. Additionally, it may allow for the implementation of other advanced features. ",
-    }
-
-   /* {
-      number: 2,
-      imageUrl: Km,
-      title: "Motor Selector",
-      link: "https://dc-motor-selection-tool-react.vercel.app",
-
-      description:
-        "This project is significant to me because it was the first application I created after mastering state and props. Its inclusion demonstrates my growth as a developer from my initial stages to the present.",
+        "After building several clones of websites that already exist, I wanted to build something that was my own original concept. When my dad retired in 2022, he was having trouble looking for new things or hobbies to occupy his time, and that is when I thought of creating Hobby Hunter. Hobby Hunter is a web application that enables users to create a personal profile and share their hobbies along with instructions on how to get started with a hobby. The app is built using REACT Redux and features Google authentication for secure login and logout. Additionally, the app utilizes Firebase Storage to store the hobbies that users may post. Even though the app has been deployed to production, it is still a work in progress as I am currently expanding the backend using Node.js, Express, and MongoDB. Building out the backend will provide additional opportunities, such as the ability to modify a user's profile after creation and the ability to engage in conversation with other users. Additionally, it may allow for the implementation of other advanced features.",
     },
-
-    {
-      number: 3,
-      title: "Coming soon: MERN REACT JS project (WIP, Original Idea)",
-      link: "github.com/buckoon",
-      description: 
-      "For Website clones and other projects please checkout my github."
-        
-      
-    }
-    */
-        
-
-   
-    // Add more projects here
   ];
 
   return (
